a25426b8f4195b0eb6b74c6066614527ee3cc373
Kasper Høglund <hoglund003@gmail.com>
2022-08-05 16:45:42 +0200

    rules and function judge added

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
+var rules = {
+    "rock": "scissors",
+    "scissors": "paper",
+    "paper": "rock"
+};
 
-function getComputerChoice() {
-    var n = Math.ceil(Math.random() * 3);
-    return n
-}
-
-function getPlayerChoice() {
-    choice = prompt("Please enter your choice").toLowerCase();
-    if (choice === "paper") {
-        choice = 1;
-    } else if (choice === "rock") {
-        choice = 2;
-    } else if (choice === "scissors") {
-        choice = 3;
+function judge(player, computer) {
+    var winner = "";
+    if (player === computer) {
+        winner = "none";
+    } else if (rules[player] === computer) {
+        winner = `player with ${player} against ${computer}`;
+    } else if (rules[computer] === player) {
+        winner = `computer with ${computer} against ${player}`;
     }
-    return choice;
-}
 
-function getWinner(playerChoice, computerChoice) {
-    if (playerChoice < computerChoice) {
-        // player wins
-        console.log("Player wins");
-    } else if (playerChoice > computerChoice) {
-        // computer wins
-        console.log("Computer wins");
-    } else if (playerChoice === computerChoice) {
-        // it's a draw
-        console.log("It's a draw");
-    }
+    console.log(`The winner is ${winner}`);
 }
