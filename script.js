8a8803b4e0bca7740b23e373e56df85b28823847
Kasper Høglund <hoglund003@gmail.com>
2022-08-05 14:24:33 +0200

    function getPlayerChoice added

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
 function getComputerChoice() {
-    var choices = ["Rock", "Paper", "Scissors"];
     var n = Math.floor(Math.random() * 3);
-    return choices[n]
+    return n
 }
+
+function getPlayerChoice() {
+    choice = prompt("Please enter your choice").toLowerCase();
+    if (choice === "paper") {
+        choice = 1;
+    } else if (choice === "rock") {
+        choice = 2;
+    } else if (choice === "scissors") {
+        choice = 3;
+    }
+    return choice;
+}
+
